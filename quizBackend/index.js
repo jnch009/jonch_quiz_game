e9ab925623846a8f6690e0ddd1767efc7b2b7b6f
jnch009 <jngcheng@sfu.ca>
2020-05-20 23:33:22 -0700

    logging csvFilePath

--- quizBackend/index.js
+++ quizBackend/index.js
@@ -1,20 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3001;
-const path = require("path");
-const csvFilePath = path.resolve(__dirname, "./public/quizQuestions.csv");
-const csv = require("csvtojson");
-const cors = require("cors");
+const path = require('path');
+const csvFilePath = path.resolve(__dirname, './public/quizQuestions.csv');
+const csv = require('csvtojson');
+const cors = require('cors');
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (req, res) =>
+app.get('/', (req, res) => {
+  console.log(csvFilePath);
   csv()
     .fromFile(csvFilePath)
     .then(jsonObj => {
       res.send(jsonObj);
-    })
-);
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
