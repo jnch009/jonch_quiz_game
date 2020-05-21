cd86e3a1195eba34abc5840c8015ff046d93e424
jnch009 <jngcheng@sfu.ca>
2020-05-21 00:20:53 -0700

    Updates

---
+++ precache-manifest.2a9f82a10bf5137a88f0024f7451eb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399807587d6060328366213edc0e2661",
+    "url": "/jonch_quiz_game/index.html"
+  },
+  {
+    "revision": "836964cad7d70593e784",
+    "url": "/jonch_quiz_game/static/css/main.72130b91.chunk.css"
+  },
+  {
+    "revision": "80374c4149f9707d5be5",
+    "url": "/jonch_quiz_game/static/js/2.559d0036.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/jonch_quiz_game/static/js/2.559d0036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836964cad7d70593e784",
+    "url": "/jonch_quiz_game/static/js/main.554e72a8.chunk.js"
+  },
+  {
+    "revision": "f79c70f42cc6113bbc70",
+    "url": "/jonch_quiz_game/static/js/runtime-main.087dd73f.js"
+  }
+]);
